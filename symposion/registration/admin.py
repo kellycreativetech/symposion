561a3a4fc72661f84c3e04b8bc38ec2212a459a3
Issac Kelly <issac.kelly@gmail.com>
2011-05-10 19:20:58 -0400

    Add Registrant admin

---
+++ symposion/registration/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from symposion.registration.models import Registrant
+
+class RegistrantAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", "will_buy_tshirt", "tshirt_size"]
+
+admin.site.register(Registrant, RegistrantAdmin)
