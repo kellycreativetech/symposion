0fb4116417dbe6aa813e6b19bca398574960e8f9
Issac Kelly <issac.kelly@gmail.com>
2012-07-17 00:39:18 -0700

    new field in mgmt command download

--- symposion/registration/management/commands/registrant_csv.py
+++ symposion/registration/management/commands/registrant_csv.py
@@ -7,6 +7,16 @@ import sys
 class Command(BaseCommand):
     def handle(self, *args, **options):
         writer = csv.writer(sys.stdout)
+        writer.writerow([
+                "First Name",
+                "Last Name",
+                "Email",
+                "Will buy shirt",
+                "Will volunteer",
+                "Shirt Size",
+                "IP",
+                "Location",
+            ])
         registrants = Registrant.objects.all()
         for r in registrants:
             writer.writerow([
@@ -14,6 +24,7 @@ class Command(BaseCommand):
                 r.last_name,
                 r.email,
                 r.will_buy_tshirt,
+                r.will_volunteer,
                 r.get_tshirt_size_display(),
                 r.remote_ip,
                 r.location,
